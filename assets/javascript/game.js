b56e1b93daaab99da565c4a637b164d5df83e776
Hassan Ali Khan <hassanalikhan87@gmail.com>
2018-10-01 02:36:12 -0700

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,91 +1,81 @@
 $( document ).ready(function(){
-    var Random=Math.floor(Math.random()*101+19)
-    // Selects a random number to be shown at the start of the game value ranging between 19 - 120
-    $('#randomNumber').text(Random);
-    // Appending random number to the randomNumber id in the html doc
-    var num1= Math.floor(Math.random()*11+1)
-    var num2= Math.floor(Math.random()*11+1)
-    var num3= Math.floor(Math.random()*11+1)
-    var num4= Math.floor(Math.random()*11+1)
-    // Setting up random numbers for each jewel ranging between 1 - 12 
-    var userTotal= 0; 
-    var wins= 0;
-    var losses = 0;
-    //  Declaring variables results
-  $('#numberWins').text(wins);
-  $('#numberLosses').text(losses);
-  //resetting the game
-  function reset(){
-        Random=Math.floor(Math.random()*101+19);
-        console.log(Random)
-        $('#randomNumber').text(Random);
-        num1= Math.floor(Math.random()*11+1);
-        num2= Math.floor(Math.random()*11+1);
-        num3= Math.floor(Math.random()*11+1);
-        num4= Math.floor(Math.random()*11+1);
-        userTotal= 0;
-        $('#finalTotal').text(userTotal);
-        } 
-  //adds the wins to the userTotal
-  function winner(){
-  alert("You won!");
-    wins++; 
-    $('#numberWins').text(wins);
-    reset();
-  }
-  //addes the losses to the userTotal
-  function loser(){
-  alert ("You lose!");
-    losses++;
-    $('#numberLosses').text(losses);
-    reset()
-  }
-  //sets up click for jewels
-    $('#one').on ('click', function(){
-      userTotal = userTotal + num1;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal); 
-            //sets win/lose conditions
-          if (userTotal == Random){
-            winner();
-          }
-          else if ( userTotal > Random){
-            loser();
-          }   
-    })  
-    $('#two').on ('click', function(){
-      userTotal = userTotal + num2;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal); 
-          if (userTotal == Random){
-            winner();
-          }
-          else if ( userTotal > Random){
-            loser();
-          } 
-    })  
-    $('#three').on ('click', function(){
-      userTotal = userTotal + num3;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal);
-  //sets win/lose conditions
-            if (userTotal == Random){
-            winner();
-          }
-          else if ( userTotal > Random){
-            loser();
-          } 
-    })  
-    $('#four').on ('click', function(){
-      userTotal = userTotal + num4;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal); 
-        
-            if (userTotal == Random){
-            winner();
-          }
-          else if ( userTotal > Random){
-            loser();
-          }
-    });   
-  }); 
+    var randomNum = Math.floor(Math.random()*101+19);
+    wins=0;
+    losses=0;
+    $("#randomDiv").text(randomNum);
+    var totalScore=0;
+    blueNum=Math.floor(Math.random()*11+1);
+    greenNum=Math.floor(Math.random()*11+1);
+    redNum=Math.floor(Math.random()*11+1);
+    yellowNum=Math.floor(Math.random()*11+1);
+    function reset(){
+        randomNum = Math.floor(Math.random()*101+19);
+        $("#randomDiv").text(randomNum);
+        totalScore=0;
+        $("#finalTotal").text(totalScore);
+        blueNum=Math.floor(Math.random()*11+1);
+        greenNum=Math.floor(Math.random()*11+1);
+        redNum=Math.floor(Math.random()*11+1);
+        yellowNum=Math.floor(Math.random()*11+1);
+
+    }
+    function win(){
+        wins++;
+        alert("You Win!");
+        $("#numberWins").text(wins);
+        reset();
+    };
+
+    function lose(){
+        alert("You Lose!");
+        losses++;
+        $("#numberLosses").text(losses);
+        reset();
+
+    }
+
+    $("#one").on("click", function(){
+        totalScore=totalScore+blueNum;
+        $("#finalTotal").text(totalScore);
+        console.log(blueNum);
+        if(totalScore == randomNum){
+            win();
+        }
+        else if(totalScore > randomNum){
+            lose();
+        }
+    });
+    $("#two").on("click", function(){
+        totalScore=totalScore+greenNum;
+        $("#finalTotal").text(totalScore);
+        console.log(greenNum);
+        if(totalScore == randomNum){
+            win();
+        }
+        else if(totalScore > randomNum){
+            lose();
+        }
+    });
+    $("#three").on("click", function(){
+        totalScore=totalScore+redNum;
+        $("#finalTotal").text(totalScore);
+        console.log(redNum);
+        if(totalScore == randomNum){
+            win();
+        }
+        else if(totalScore > randomNum){
+            lose();
+        }
+    });
+    $("#four").on("click", function(){
+        totalScore=totalScore+yellowNum;
+        $("#finalTotal").text(totalScore);
+        console.log(yellowNum);
+        if(totalScore == randomNum){
+            win();
+        }
+        else if(totalScore > randomNum){
+            lose();
+        }
+    });
+})
